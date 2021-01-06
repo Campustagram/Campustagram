a9becd5cba427cb1797d73d6adbda2dadbc6bb29
Ahmet Eren Incir <33928836+AhmetErenIncir@users.noreply.github.com>
2021-01-06 21:39:20 +0300

    Unit Test
    Unit Test of AppCommonFunctions class methods.

--- src/main/java/com/campustagram/app/common/AppCommonFunctionsTest.java
+++ src/main/java/com/campustagram/app/common/AppCommonFunctionsTest.java
@@ -5,9 +5,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class AppCommonFunctionsTest {
-	
+
 	AppCommonFunctions appcommonfunctions = new AppCommonFunctions();
-	
 	
 	@SuppressWarnings("static-access")
 	@Test
@@ -15,44 +14,44 @@ class AppCommonFunctionsTest {
 	 * checkTheValidityOfTheIntegerVariableAndWriteErrorMessageValidVariable methodunun upperLimit değerinden daha büyük bir değer ile testi.
 	 */
 	void checkTheValidityOfTheIntegerVariableAndWriteErrorMessageGreaterUpperLimitVariableTest() {
-		
+
 		assertThrows(Exception.class,() -> appcommonfunctions.checkTheValidityOfTheIntegerVariableAndWriteErrorMessage(30, "testname", 3, 20, "lowerLimitErrorMessage", "upperLimitErrorMessage", true));
 
 	}
-	
+
 	@SuppressWarnings("static-access")
 	@Test
 	/**
 	 * checkTheValidityOfTheIntegerVariableAndWriteErrorMessageValidVariable methodunun lower limit değerinden daha küçük bir değer ile testi.
 	 */
 	void checkTheValidityOfTheIntegerVariableAndWriteErrorMessageLessLowerLimitVariableTest() {
-		
+
 		assertThrows(Exception.class,() -> appcommonfunctions.checkTheValidityOfTheIntegerVariableAndWriteErrorMessage(0, "testname", 3, 20, "lowerLimitErrorMessage", "upperLimitErrorMessage", true));
 
 	}
-	
+
 	@SuppressWarnings("static-access")
 	@Test
 	/**
 	 * checkTheValidityOfTheIntegerVariableAndWriteErrorMessageValidVariable methodunun doğru parametreler ile testi
 	 */
 	void checkTheValidityOfTheIntegerVariableAndWriteErrorMessageValidVariableTest() {
-		
-		boolean result = appcommonfunctions.checkTheValidityOfTheIntegerVariableAndWriteErrorMessage(5, "testname", 3, 20, "lowerLimitErrorMessage", "upperLimitErrorMessage", true); 
+
+		boolean result = appcommonfunctions.checkTheValidityOfTheIntegerVariableAndWriteErrorMessage(5, "testname", 3, 20, "lowerLimitErrorMessage", "upperLimitErrorMessage", true);
 		assertEquals(true,result);
 	}
-	
+
 	@SuppressWarnings("static-access")
 	@Test
 	/**
 	 * checkTheValidityOfTheIntegerVariableAndWriteErrorMessage methodunun null testi.
 	 */
 	void checkTheValidityOfTheIntegerVariableAndWriteErrorMessageNullVariableTest() {
-		
-		boolean result = appcommonfunctions.checkTheValidityOfTheIntegerVariableAndWriteErrorMessage(null, "testname", 3, 20, "lowerLimitErrorMessage", "upperLimitErrorMessage", false); 
+
+		boolean result = appcommonfunctions.checkTheValidityOfTheIntegerVariableAndWriteErrorMessage(null, "testname", 3, 20, "lowerLimitErrorMessage", "upperLimitErrorMessage", false);
 		assertEquals(true,result);
 	}
-	
+
 	@SuppressWarnings("static-access")
 	@Test
 	/**
@@ -62,7 +61,7 @@ class AppCommonFunctionsTest {
 		boolean result = appcommonfunctions.checkTheValidityOfTheStringVariableAndWriteErrorMessage(null, "variableName", 3, 20, "lowerLimitErrorMessage", "upperLimitErrorMessage", false);
 		assertEquals(true,result);
 	}
-	
+
 	@SuppressWarnings("static-access")
 	@Test
 	/**
@@ -72,7 +71,7 @@ class AppCommonFunctionsTest {
 		boolean result = appcommonfunctions.checkTheValidityOfTheStringVariableAndWriteErrorMessage("deneme", "variableName", 3, 20, "lowerLimitErrorMessage", "upperLimitErrorMessage", false);
 		assertEquals(true,result);
 	}
-	
+
 	@SuppressWarnings("static-access")
 	@Test
 	/**
@@ -89,10 +88,10 @@ class AppCommonFunctionsTest {
 	void checkTheValidityOfTheStringVariableAndWriteErrorMessageLessThanLowerLimitVariableTest() {
 		assertThrows(Exception.class, () -> appcommonfunctions.checkTheValidityOfTheStringVariableAndWriteErrorMessage("a", "variableName", 3, 5, "lowerLengthErrorMessage", "upperLengthErrorMessage", false));
 	}
-	
-	
-	
-	
-	
+
+
+
+
+
 
 }
